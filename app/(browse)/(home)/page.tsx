c217e79af571243236f15ea2f614253dc4c09925
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2024-01-18 15:50:40 -0300

    Home page created

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,5 +1,15 @@
+import { Suspense } from "react";
+
+import { Results, ResultsSkeleton } from "./_components/results";
+
 const RootPage = () => {
-  return <></>;
+  return (
+    <div className="h-full p-8 max-w-screen-2xl mx-auto">
+      <Suspense fallback={<ResultsSkeleton />}>
+        <Results />
+      </Suspense>
+    </div>
+  );
 };
 
 export default RootPage;
